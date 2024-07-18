8b4dbe6800f09ae8a57d2234987422ff3850f448
PAxNova <votre.email@example.com>
2024-07-18 23:09:29 +0200

    Maj du cloudinary

---
+++ config/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = ENV['CLOUDINARY_URL'].split('@').last
+    config.api_key = ENV['CLOUDINARY_URL'].split('://').last.split(':').first
+    config.api_secret = ENV['CLOUDINARY_URL'].split(':').last.split('@').first
+    config.secure = true
+    config.cdn_subdomain = true
+  end
